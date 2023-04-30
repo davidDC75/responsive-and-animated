1b8e03cc92bb2b738c97e63b9c175bf1d5824209
David De Castro <segphault@gmail.com>
2023-04-30 19:29:13 +0200

    Fini V1

---
+++ README.md
@@ -0,0 +1,1 @@
+# Exercices sur responsive et animations
--- js/main.js
+++ js/main.js
@@ -8,8 +8,17 @@ const title = document.getElementById('title');
 const ours = document.getElementById('ours');
 const gun = document.getElementById('gun');
 
+// POUR SECTION 2
+const gridAnim1 = document.getElementById('grid-anim-1');
+const gridAnim2 = document.getElementById('grid-anim-2');
+const gridAnim3 = document.getElementById('grid-anim-3');
+const gridAnim4 = document.getElementById('grid-anim-4');
+const gridAnim5 = document.getElementById('grid-anim-5');
+const gridAnim6 = document.getElementById('grid-anim-6');
+
 const btnHeader = document.getElementById('button-header');
 const btnFooter = document.getElementById('button-footer');
+const btnSection1 = document.getElementById('button-section-1');
 
 
 // On recommence l'animation de la section 1
@@ -33,6 +42,27 @@ btnFooter.onclick = () => {
         window.requestAnimationFrame(function (time) {
             imageLogo.className='image-logo animation-image-logo';
             title.className='animation-title';
+        })
+    });
+}
+
+// On recommence l'animation de la section 2
+// si on clique sur l'ancre de la section 1
+btnSection1.onclick = () => {
+    gridAnim1.className='circle-h-grid';
+    gridAnim2.className='circle-c-grid';
+    gridAnim3.className='circle-j-grid';
+    gridAnim4.className='circle-p-grid';
+    gridAnim5.className='circle-m-grid';
+    gridAnim6.className='circle-s-grid';
+    window.requestAnimationFrame(function (time) {
+        window.requestAnimationFrame(function (time) {
+            gridAnim1.className='circle-h-grid grid-anim-1';
+            gridAnim2.className='circle-c-grid grid-anim-2';
+            gridAnim3.className='circle-j-grid grid-anim-3';
+            gridAnim4.className='circle-p-grid grid-anim-4';
+            gridAnim5.className='circle-m-grid grid-anim-5';
+            gridAnim6.className='circle-s-grid grid-anim-6';
         })
     });
 }
